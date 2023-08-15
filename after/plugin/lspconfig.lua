cc6933bdcb599d3069c2dc152367b7734207a710
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-08-15 13:50:48 +0200

    Prepare fedora build

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -41,30 +41,3 @@ lspconfig.clangd.setup {
     end
   end
 }
-
--- go
-lspconfig.gopls.setup {
-  capabilities = capabilities,
-}
-
--- lua
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file('', true),
-        checkThirdParty = false,
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
