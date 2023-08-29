f01f25e3b3262519c1c41b8860a0a6d19b0881b9
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-08-29 16:39:33 +0200

    Add json snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,36 +1,6 @@
 return {
-  -- TODO: Improve snippets configuration
   'L3MON4D3/LuaSnip',
   config = function()
-    -- variables
-    local luasnip = require('luasnip')
-    local s = luasnip.s
-
-    -- nodes
-    local i = luasnip.insert_node
-    local t = luasnip.text_node
-
-    -- snippets for cmake
-    luasnip.add_snippets('all', {
-      s('executable', {
-        t('cmake_minimum_required(VERSION '),
-        i(1, '3.20'),
-        t({ ')', '' }),
-        t({ '', 'project(' }),
-        i(2, 'project-name'),
-        t(')'),
-        t({ '', 'set(CMAKE_EXPORT_COMPILE_COMMANDS ON)', '' }),
-        t({ '', 'add_executable(main)' }),
-        t({ '', 'target_sources(main PRIVATE ' }),
-        i(3, 'src/main.c'),
-        t({ ')', '' })
-      }),
-      s('version', {
-        t('target_compile_features(main PUBLIC cxx_std_'),
-        i(1, '17'),
-        t({ ')', '' }),
-        t({ 'set_target_properties(main PROPERTIES CXX_EXTENSIONS OFF)', '' })
-      })
-    })
+    require('luasnip.loaders.from_vscode').load({ paths = { "./json/snippets/" } })
   end,
 }
