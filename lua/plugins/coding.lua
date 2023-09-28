6c2d815659a2fdf736244333ab34680d6c06e93c
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-09-28 12:53:13 +0200

    Move luasnip into coding.lua

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,7 +1,8 @@
 return {
   -- TODO: Improve cmp configuration
   'hrsh7th/nvim-cmp',
-  dependencies = {
+  dependencies = {  
+    'L3MON4D3/LuaSnip',
     'hrsh7th/cmp-nvim-lsp',
     'saadparwaiz1/cmp_luasnip',
   },
