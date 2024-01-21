fc92049a2b112e704556306c3d322a2bbe13d4bf
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-01-21 12:24:55 +0100

    Remove C/C++ support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -29,42 +29,9 @@ return {
             signs = false
         })
 
-        -- clangd
-        lspconfig.clangd.setup {
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--fallback-style=webkit"
-            },
-            on_init = function(client)
-                local request = client.rpc.request
-                function client.rpc.request(method, params, handler, ...)
-                    if method ~= 'textDocument/completion' then
-                        return request(method, params, handler, ...)
-                    end
-                    local new_handler = function(...)
-                        local err, result = ...
-                        if err or not result then
-                            return handler(...)
-                        end
-                        local items = result.items or result
-                        for _, item in ipairs(items) do
-                            local kind = vim.lsp.protocol.CompletionItemKind
-                            if item.kind == kind.Snippet then
-                                local text = item.textEdit.newText
-                                text = text:gsub('{\n', '{\n\t')
-                                item.textEdit.newText = text
-                            end
-                        end
-                        return handler(...)
-                    end
-                    return request(method, params, new_handler, ...)
-                end
-            end,
-        }
-
         -- haskell
         lspconfig.hls.setup {
+            capabilities = capabilities,
             settings = {
                 haskell = {
                     formattingProvider = "fourmolu"
@@ -72,7 +39,7 @@ return {
             }
         }
 
-        -- rust analyzer
+        -- rust
         lspconfig.rust_analyzer.setup {
             capabilities = capabilities,
             settings = {
