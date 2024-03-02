92116450cc1f4aa1ebf80b017b04c28f358378cd
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-03-02 17:30:01 +0100

    Add shortcuts for vim-fugitive
    Added:
    - Commit shortcut `<leader>gc`
    - Push shortcut `<leader>gp`
    - Pull shortcut `<leader>gP`

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,25 @@
 return {
     {
         "tpope/vim-fugitive",
+        keys = {
+            {
+                "<leader>gc",
+                function()
+                    vim.cmd.Git("commit")
+                end
+            },
+            {
+                "<leader>gp",
+                function()
+                    vim.cmd.Git("push")
+                end
+            },
+            {
+                "<leader>gP",
+                function()
+                    vim.cmd.Git("pull")
+                end
+            }
+        }
     }
 }
