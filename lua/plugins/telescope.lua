1bd3bcc91efd147ddd8fbb8c4ca438ff2b5897af
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-03-08 13:05:34 +0100

    Update telescope from `0.1.4` to `0.1.5`
    Update to the latest release of telescope [1].
    
    [1] https://github.com/nvim-telescope/telescope.nvim/releases

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        version = "0.1.4",
+        version = "0.1.5",
         dependencies = { "nvim-lua/plenary.nvim" },
         keys = {
             {
