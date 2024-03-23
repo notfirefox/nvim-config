0b50570994a8ac9cc6a480ba2fd7f4a0fd8b7b29
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-03-23 21:52:36 +0100

    Update plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        version = "0.1.5",
+        version = "0.1.6",
         dependencies = { "nvim-lua/plenary.nvim" },
         keys = {
             {
