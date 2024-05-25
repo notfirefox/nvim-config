4600caea07a433bbf546beed27e170ce79e69447
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-05-25 13:56:01 +0000

    Add `ensure_installed` table to treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,18 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "haskell",
+                "lua",
+                "markdown",
+                "query",
+                "rust",
+                "vim",
+                "vimdoc",
+            },
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
