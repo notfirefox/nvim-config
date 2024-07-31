9a9181f0498fb8db6785658841068e9423742c6d
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-07-31 22:44:41 +0200

    Restructure nvim config

---
+++ lua/core/config.lua
@@ -0,0 +1,27 @@
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- tab settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- undo settings
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+vim.opt.undofile = true
+
+-- no wrapping
+vim.opt.wrap = false
+
+-- visualize tabs
+vim.opt.list = true
+vim.opt.listchars = "tab:>-"
+
+-- color column
+vim.opt.colorcolumn = "80"
+
+-- set leader key
+vim.g.mapleader = " "
