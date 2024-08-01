648b9e0025b9d8d8c0af0747f6fbf67d0f2b15bc
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-08-01 13:07:53 +0200

    Replace `vim.loop.fs_stat` with `vim.uv.fs_stat`

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -70,8 +70,8 @@ return {
             capabilities = capabilities,
             on_init = function(client)
                 local path = client.workspace_folders[1].name
-                if vim.loop.fs_stat(path .. "/.luarc.json") or
-                    vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+                if vim.uv.fs_stat(path .. "/.luarc.json") or
+                    vim.uv.fs_stat(path .. "/.luarc.jsonc") then
                     return
                 end
                 client.config.settings.Lua = vim.tbl_deep_extend(
@@ -83,7 +83,8 @@ return {
                         workspace = {
                             checkThirdParty = false,
                             library = {
-                                vim.env.VIMRUNTIME
+                                vim.env.VIMRUNTIME,
+                                "${3rd}/luv/library"
                             }
                         }
                     })
