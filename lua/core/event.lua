dc34b5d10590499bb720a9859f2f1725e57c414e
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-08-28 22:20:21 +0200

    Refactor code
    Create the formatting autocmd as recommended in the documentation [1].
    
    [1] https://neovim.io/doc/user/lsp.html#lsp-config

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -30,3 +30,37 @@ vim.api.nvim_create_autocmd("ColorScheme", {
         vim.g.LAST_THEME = params.match
     end,
 })
+
+-- configure language server
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp_attach", { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then
+            return
+        end
+
+        local bind = vim.keymap.set
+        local methods = vim.lsp.protocol.Methods
+        local opts = { buffer = args.buf }
+        local utils = require("core.utils")
+
+        if client.supports_method(methods.textDocument_codeAction) then
+            bind("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end
+
+        if client.supports_method(methods.textDocument_hover) then
+            bind("n", "K", vim.lsp.buf.hover, opts)
+        end
+
+        if client.supports_method(methods.textDocument_formatting)
+        then
+            utils.lsp.add_formatting_autocmd(args.buf, client.id)
+            utils.lsp.create_clang_format()
+        end
+
+        if client.supports_method(methods.textDocument_rename) then
+            bind("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        end
+    end,
+})
---
+++ lua/core/utils.lua
@@ -0,0 +1,25 @@
+local utils = {}
+
+-- lsp submodule
+utils.lsp = {}
+
+-- add formatting auto command
+function utils.lsp.add_formatting_autocmd(bufnr, client_id)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr, id = client_id })
+        end
+    })
+end
+
+-- create clang format file
+function utils.lsp.create_clang_format()
+    local file = vim.fn.expand("~/.clang-format")
+    if not vim.uv.fs_stat(file) then
+        local text = { "---", "BasedOnStyle: LLVM", "IndentWidth: 4" }
+        vim.fn.writefile(text, file)
+    end
+end
+
+return utils
