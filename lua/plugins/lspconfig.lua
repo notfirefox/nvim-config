d003eef7c83ec31ad2a55d19c2f565b1a93cc72a
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-08-28 22:20:21 +0200

    Refactor code
    Create the formatting autocmd as recommended in the documentation [1].
    
    [1] https://neovim.io/doc/user/lsp.html#lsp-config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,36 +1,9 @@
--- create clang-format file
-local file = vim.fn.expand("~/.clang-format")
-if not vim.uv.fs_stat(file) then
-    local text = { "---", "BasedOnStyle: LLVM", "IndentWidth: 4" }
-    vim.fn.writefile(text, file)
-end
-
 return {
     "neovim/nvim-lspconfig",
     config = function()
         -- variables
         local lspconfig = require("lspconfig")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        -- auto format on save
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("format_code", {}),
-            callback = function()
-                vim.lsp.buf.format { async = false }
-            end
-        })
-
-        -- set lsp bindings
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("lsp_attach", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf }
-                local bind = vim.keymap.set
-                bind("n", "K", vim.lsp.buf.hover, opts)
-                bind("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                bind("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-            end,
-        })
 
         -- disable diagnostic signs
         vim.diagnostic.config({
