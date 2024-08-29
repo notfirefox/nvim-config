46ff7f43dc8a311ed6bd815e34faee8af8b58807
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-08-29 16:47:35 +0200

    Use snippets for lua functions
    Add snippet support for functions in Lua [1].
    
    [1] https://github.com/LuaLS/lua-language-server/wiki/Settings#completioncallsnippet

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -67,6 +67,9 @@ return {
                 client.config.settings.Lua = vim.tbl_deep_extend(
                     "force",
                     client.config.settings.Lua, {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
                         runtime = {
                             version = "LuaJIT"
                         },
